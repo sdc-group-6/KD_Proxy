cc49699356c123215d2af27368cac32dfd84210b
Kelly Hudiono <kellyhudiono@gmail.com>
2019-02-21 11:00:42 -0800

    bundle all js files with gulp + fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var concatCss = require('gulp-concat-css');
 
 gulp.task('css', function() {
@@ -7,3 +8,9 @@ gulp.task('css', function() {
     .pipe(concatCss('styleCombined.css'))
     .pipe(gulp.dest('./proxy/public'));
 });
+
+gulp.task('bundle', () => {
+  return gulp.src(['./product-view/public/bundle.js', './product-details/public/bundle.js'])
+    .pipe(concat('allBundles.js'))
+    .pipe(gulp.dest('./proxy/public'));
+})
